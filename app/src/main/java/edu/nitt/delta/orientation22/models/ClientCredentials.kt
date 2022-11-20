3e6f4edb9de3de924cace590e022e55a964a2fd3
Ram-20062003 <ramajs2003@gmail.com>
2022-11-20 16:53:56 +0530

    remove: Remove redundant code

--- app/src/main/java/edu/nitt/delta/orientation22/models/ClientCredentials.kt
+++ app/src/main/java/edu/nitt/delta/orientation22/models/ClientCredentials.kt
@@ -2,8 +2,7 @@ package edu.nitt.delta.orientation22.models
 
 open class ClientCredentials (val param : String) {
     object ClientID : ClientCredentials("")
-    object RedirectUri : ClientCredentials("https://www.google.com/")
+    object RedirectUri : ClientCredentials("")
     object State : ClientCredentials("")
     object Nonce : ClientCredentials("")
-    object ClientSecret : ClientCredentials("")
 }
