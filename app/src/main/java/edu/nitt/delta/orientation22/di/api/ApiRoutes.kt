a07a8d5aed8c8c8673134e39afdf4f3503888a90
sarvesh-03 <sarveshjan18@gmail.com>
2022-11-23 14:06:28 +0530

    fix(api):change backend url

--- app/src/main/java/edu/nitt/delta/orientation22/di/api/ApiRoutes.kt
+++ app/src/main/java/edu/nitt/delta/orientation22/di/api/ApiRoutes.kt
@@ -1,13 +1,13 @@
 package edu.nitt.delta.orientation22.di.api
 
 object ApiRoutes {
-    const val BASE_URL = "http://deltaforce.club"
+    const val BASE_URL = "https://utopia.nitt.edu/OT22/"
 
-    const val LEADERBOARD = "/api/leaderboard"
-    const val REGISTER_TEAM ="/api/registerTeam"
-    const val TEAM="/api/team"
-    const val ROUTE ="/api/route"
-    const val CURRENT_STATE="/api/currentState"
-    const val NEXT_STATE="/api/nextState"
-    const val AUTH_CALLBACK = "/api/auth/callback"
+    const val LEADERBOARD = "api/leaderboard"
+    const val REGISTER_TEAM ="api/registerTeam"
+    const val TEAM="api/team"
+    const val ROUTE ="api/route"
+    const val CURRENT_STATE="api/currentState"
+    const val NEXT_STATE="api/nextState"
+    const val AUTH_CALLBACK = "api/auth/callback"
 }
--- app/src/main/java/edu/nitt/delta/orientation22/models/ClientCredentials.kt
+++ app/src/main/java/edu/nitt/delta/orientation22/models/ClientCredentials.kt
@@ -2,7 +2,7 @@ package edu.nitt.delta.orientation22.models
 
 open class ClientCredentials (val param : String) {
     object ClientID : ClientCredentials("U7f5kKtVq5WOIcXu")
-    object RedirectUri : ClientCredentials("http://deltaforce.club/api/auth/callback")
+    object RedirectUri : ClientCredentials("https://utopia.nitt.edu/OT22/api/auth/callback")
     object State : ClientCredentials("state")
     object Nonce : ClientCredentials("123456")
 }
