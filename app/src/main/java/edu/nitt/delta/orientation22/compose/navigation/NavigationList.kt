22ffd21105defc5559c09c894e9c82efc54b1335
DonWIck32 <nitish2k03@gmail.com>
2023-08-25 23:24:16 +0530

    feat: add new nav bar

--- app/src/main/java/edu/nitt/delta/orientation22/compose/navigation/NavigationList.kt
+++ app/src/main/java/edu/nitt/delta/orientation22/compose/navigation/NavigationList.kt
@@ -1,22 +1,32 @@
 package edu.nitt.delta.orientation22.compose.navigation
 
 import edu.nitt.delta.orientation22.R
+import edu.nitt.delta.orientation22.ui.theme.brightYellow
+import edu.nitt.delta.orientation22.ui.theme.cyan
+import edu.nitt.delta.orientation22.ui.theme.lightCyan
+import edu.nitt.delta.orientation22.ui.theme.red
 
 sealed class NavigationList(val navList: List<BottomNavItem>) {
     object NavList : NavigationList(
         listOf(
-        BottomNavItem(
-            name = "Home",
-            route = NavigationRoutes.Dashboard.route,
-            selectedIcon = R.drawable.home_screen_selected,
-            unselectedIcon = R.drawable.home_screen_unselected
-        ),
-        BottomNavItem(
-            name = "Leaderboard",
-            route = NavigationRoutes.LeaderBoard.route,
-            selectedIcon = R.drawable.leaderboard_selected,
-            unselectedIcon = R.drawable.leaderboard_unselected
-        ),
+            BottomNavItem(
+                name = "Home",
+                route = NavigationRoutes.Dashboard.route,
+                icon = R.drawable.home_screen_selected,
+                color = lightCyan
+            ),
+            BottomNavItem(
+                name = "Map",
+                route = NavigationRoutes.Map.route,
+                icon = R.drawable.map_screen_selected,
+                color = red
+            ),
+            BottomNavItem(
+                name = "Leaderboard",
+                route = NavigationRoutes.LeaderBoard.route,
+                icon = R.drawable.leaderboard_selected,
+                color = brightYellow
+            ),
         ),
     )
 }
--- app/src/main/java/edu/nitt/delta/orientation22/ui/theme/Theme.kt
+++ app/src/main/java/edu/nitt/delta/orientation22/ui/theme/Theme.kt
@@ -61,7 +61,8 @@ fun Orientation22androidTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = brown.toArgb()
+            // change status bar color
+            (view.context as Activity).window.statusBarColor = darkBlue.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
